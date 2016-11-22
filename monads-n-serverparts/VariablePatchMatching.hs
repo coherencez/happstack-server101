630007e969964e85a3d2a792d246299c1cec139d
Thomas Swann <tkswann@mac.com>
2016-11-22 14:15:19 -0600

    extending path with instance of FromReqURI -- possible route security??

--- monads-n-serverparts/VariablePatchMatching.hs
+++ monads-n-serverparts/VariablePatchMatching.hs
@@ -1,9 +1,25 @@
 module Main where
-  import Control.Monad    (msum)
-  import Happstack.Server (nullConf, simpleHTTP, ok, dir, path, seeOther)
+import Control.Monad    (msum)
+import Data.Char        (toLower)
+import Happstack.Server ( FromReqURI(..), nullConf, simpleHTTP
+                        , ok, dir, path, seeOther
+                        )
+-- creating a type to represet subjects to greet
+data Subject = World | Haskell
 
-  main :: IO ()
-  main = simpleHTTP nullConf $
-    msum [ dir "hello" $ path $ \s -> ok $ "Hello, " ++ s
-         , seeOther "/hello/Haskell" "/hello/Haskell"
-         ]
+sayHello :: Subject -> String
+sayHello World   = "Hello, World!"
+sayHello Haskell = "Greetings, Haskell!"
+
+instance FromReqURI Subject where
+    fromReqURI sub =
+      case map toLower sub of
+        "haskell" -> Just Haskell
+        "world"   -> Just World
+        _         -> Nothing
+
+main :: IO ()
+main = simpleHTTP nullConf $
+  msum [ dir "hello" $ path $ \subject -> ok $ (sayHello subject)
+       , seeOther "/hello/World" "/hello/World"
+       ]
